3e786aff5867729983efdd39fb174ae728b6860c
Desired <95721305+DesiredToResign@users.noreply.github.com>
2024-10-17 16:53:04 -0600

    added stuff

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("WELCOME TO HACKING SIMULATOR!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hacking_Simulator 
+{
+    class Program
+    {
+        string response = Console.ReadKey(true).KeyChar.ToString().ToLower();
+        public static void Main(string[] args)
+        {
+            Start();
+        }
+
+        public static void Start() 
+        {
+            Console.Clear();
+            Console.WriteLine("WELCOME TO HACKING SIMULATOR!");
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE");
+            Console.ReadKey(true);
+            Console.Clear();
+            MainProgram(); 
+        }
+
+        public static void MainProgram()
+        {
+            Console.WriteLine("Let's Get Started...");
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE");
+            Console.ReadKey(true);
+            Console.Clear();
+
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacking_Simulator
+{
+    public class Utility
+    {
+        public static Random rand = new Random();
+        public static void KeyPress()
+        {
+            Console.WriteLine("\nHit Any Key To continue");
+            Console.ReadKey(true);
+        }
+        public static int Int(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+    }
+}
